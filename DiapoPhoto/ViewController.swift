fa0f244cbb4aa06358de1d1b2d7e0161caf8cde1
Etienne Jézéquel <etiennejezequel@MacBook-Pro-de-Etienne.local>
2017-01-04 22:46:01 +0100

    First Commit

---
+++ DiapoPhoto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DiapoPhoto
+//
+//  Created by Etienne Jézéquel on 04/01/2017.
+//  Copyright © 2017 Etienne Jézéquel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
